ec018a1d4540dc51f704bfa458a12e116af486cd
Abram Boutros <abramboutros96@gmail.com>
2022-05-27 01:16:36 +0200

    deployment try 7 removed unexisting import in shared/loader

--- components/shared/loader/Loader.js
+++ components/shared/loader/Loader.js
@@ -1,25 +1,28 @@
 import React from "react";
 
-import classes from "./loader.module.css";
 import { useSelector } from "react-redux";
 
 function Loader() {
-  const loading = useSelector((state) => state.loader.loader);
-  let circleCommonClasses = "h-2.5 w-2.5 bg-current   rounded-full";
+	const loading = useSelector((state) => state.loader.loader);
+	let circleCommonClasses = "h-2.5 w-2.5 bg-current   rounded-full";
 
-  return (
-    <>
-      {loading && (
-        <div className="flex justify-center items-center">
-          <div className={`${circleCommonClasses} mr-1 mt-1 animate-bounce`}></div>
-          <div
-            className={`${circleCommonClasses} mr-1 mt-1 animate-bounce200`}
-          ></div>
-          <div className={`${circleCommonClasses} mt-1 animate-bounce400`}></div>
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			{loading && (
+				<div className="flex justify-center items-center">
+					<div
+						className={`${circleCommonClasses} mr-1 mt-1 animate-bounce`}
+					></div>
+					<div
+						className={`${circleCommonClasses} mr-1 mt-1 animate-bounce200`}
+					></div>
+					<div
+						className={`${circleCommonClasses} mt-1 animate-bounce400`}
+					></div>
+				</div>
+			)}
+		</>
+	);
 }
 
 export default Loader;
