3bfb31b59576753b28576f0c4ba01afdfdcb7398
Mohd Rejoan <wearecrx@gmail.com>
2022-06-14 19:52:22 +0600

    new font-style added

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,31 @@ module.exports = {
     theme: {
         extend: {
             animation: {
-               
+                // old 1.0
+               bounce200: "bounce 1s infinite 200ms",
+                bounce400: "bounce 1s infinite 400ms",
             },
             maxWidth: {
-                
+                // old 1.0
+                    "max-container": "1370px",
             },
             colors: {
+                // old 1.0
+                 "white-blue": "#198CFF",
+                "liquid-white": "#f3f4f8",
+                "liquid-white-50": "#F3F4F8F2",
+                "liquid-white-100": "#f7f7f9",
+                "mix-white-black": "#707070",
+                "mix-white-black-100": "#8D919F",
+                "mix-white-black-200": "#646e86",
+                "full-dark": "#0f121d",
+                "nav-text": "#47516C",
+                "almost-dark": "#0f121d",
+                "red-yellow": "#fc5900",
+                "red-yellow-gold": "#FC2300",
+                "card-border": "#141726",
+                "btn-hover": "#00ccf2",
+                // new design 2.0
                 'card-&-content-box': "#241E2C",
                 "user-color": '#D14631',
                 'buttons-gray': '#3E3847',
@@ -22,6 +41,9 @@ module.exports = {
                 'light-text': '#FFFAFF'
             },
             fontSize: {
+                // old 1.0 
+                "20px": "20px",
+                // new design 2.0 
                 "30px": '30px',
                 "25px": '25px',
                 '15px': '15px',
@@ -31,17 +53,25 @@ module.exports = {
                 '10px': '10px'
             },
             fontFamily: {
+                // old 1.0
+                "gotham-book": "gotham-book",
+                "gotham-mid": "gotham-mid",
+                "sf-pro": "sf-pro",
+                // new design 2.0 
                 'gotham': 'Gotham',
                 'NEDGEN': 'NEDGEN',
                 'SF Pro Text': 'SF Pro Text'
             },
             borderRadius: {
+                // old 1.0 
                 "5px": "5px",
                 "7px": "7px",
                 "8px": "8px"
             },
             borderWidth: {
+                // old 1.0 
                 DEFAULT: "1px",
+                "0_3": "0.3px"
             },
         },
     },
