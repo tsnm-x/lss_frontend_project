78d09f65d94f4f6682676f8f3c0be593fec21dae
tsnm-x <tasneemsm1234@gmail.com>
2022-07-02 17:08:15 +0200

    added champions

--- components/Ui/New-Components/Profile/TableElement/Table/Table.js
+++ components/Ui/New-Components/Profile/TableElement/Table/Table.js
@@ -1,14 +1,127 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import TableControlBtns from '../TableControlBtns/TableControlBtns'
 import TableHeader from '../TableHeader/TableHeader'
 import TableBodyRow from '../TableBodyRow/TableBodyRow'
 import JhinImg from '../../../../../../public/assets/new-images/Profile/Jhin.png'
 import ZiggsImg from '../../../../../../public/assets/new-images/Profile/Ziggs.png'
+import useHttp from '../../../../../../hook/useHttp'
+import { useRouter } from 'next/router'
 
 
 
 const Table = () => {
 
+  const [expand, setExpand] = useState(false);
+	const [seasonMostPlayedList, setSeasonMostPlayedList] = useState([]);
+	const {sendRequest, hasError} = useHttp();
+	const [matches, setMatches] = useState([]);
+	const [mainPlayerChamps, setMainPlayerChamps] = useState([])
+	const router = useRouter();
+	let champions  = [];
+
+	const requestHandler = (res) => {
+		if(!res){
+			console.log("no response from server");
+			return;
+		}
+		
+		setMatches(res.data.matches);
+	}
+
+  function convertM(value) {
+		const sec = parseInt(value, 10); // convert value to number if it's string
+		let minutes = Math.floor((sec * 3600) / 60); // get minutes
+		return minutes;
+	}
+
+	useEffect(()=>{
+		sendRequest(
+			{
+				url: "/seasonMostPlayed",
+				method: "GET",
+				params: { region: router.query.region, summonerName: router.query.summonerName, count: 50},
+			},
+			requestHandler
+		);
+	}, [])
+
+
+	useEffect(() => {
+
+		matches.forEach((match) => {
+			const mainPlayerArr = match.players.filter((player) => player.mainPlayer === true);
+			mainPlayerArr.forEach((obj) => {
+				champions.push({...obj, duration: match.duration})
+			})
+		})
+
+		setMainPlayerChamps(champions)
+	}, [matches])
+
+	useEffect(() => {
+		let maxcount = 0;
+		let deaths = 0;
+		let assists = 0;
+		let kills = 0;
+		let winCount = 0;
+		let lossCount = 0;
+    let totalCs = 0;
+    let totalMatches = 0;
+    let avgCs = 0;
+    let totalDuration = 0;
+
+		for (let i = 0; i < mainPlayerChamps.length; i++) {
+			let count = 0;
+			for (let j = 0; j < mainPlayerChamps.length; j++) {
+
+				if (mainPlayerChamps[i].championName == mainPlayerChamps[j].championName){
+					count++;
+					deaths = deaths + mainPlayerChamps[j].deaths;
+					assists = assists + mainPlayerChamps[j].assists;
+					kills = kills + mainPlayerChamps[j].kills;
+          totalCs = totalCs + mainPlayerChamps[j].totalMinionsKilled
+					mainPlayerChamps[j].win? winCount++ : lossCount++;
+          totalMatches++;
+          totalDuration = totalDuration + mainPlayerChamps[j].duration;
+				}
+			}
+
+			if (count > maxcount) {
+				maxcount = count;
+        avgCs = totalCs / totalMatches / convertM(totalDuration)
+				setSeasonMostPlayedList([...seasonMostPlayedList,{ ...mainPlayerChamps[i], totalDeaths: deaths, totalAssists: assists, totalKills: kills, winCount, lossCount, avgCs}]);
+			}
+
+      deaths = 0;
+			assists = 0;
+			kills = 0;
+			winCount = 0;
+			lossCount = 0;
+      totalCs = 0;
+      totalMatches = 0;
+      avgCs = 0;
+      totalDuration = 0;
+
+
+		}
+
+	}, [mainPlayerChamps])
+
+
+	useEffect(()=>{
+		console.log(seasonMostPlayedList)
+		// console.log(mainPlayerChamps)
+		const newChamps = mainPlayerChamps.filter((champion) => {
+			return champion.championName !== seasonMostPlayedList[seasonMostPlayedList.length-1].championName
+		});
+
+		if(newChamps[0]){
+			setMainPlayerChamps(newChamps);
+		}
+	}, [seasonMostPlayedList])
+
+
+  
   const tableData = [
     {
       rank: 1,
