0b1f214b3ad3e6d1dfcdffaed77c20bb8bbbc031
tsnm-x <tasneemsm1234@gmail.com>
2022-07-03 23:29:48 +0200

    fixed problem with live simulator component

--- components/HOC/old-components/LiveContentWrapHoc/LiveContentWrapHoc.js
+++ components/HOC/old-components/LiveContentWrapHoc/LiveContentWrapHoc.js
@@ -1,7 +1,5 @@
 import React from "react";
 import classes from "./LiveContentWrap.module.css";
-import Image from "next/image";
-import SuuusanoImg from "../../../public/assets/Live/suuusanoo.png";
 
 const LiveContentWrap = (PushedElement) => {
     return class NewElement extends React.Component {
