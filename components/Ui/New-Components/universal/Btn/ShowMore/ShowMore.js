29163a0c81e0d229a1395316356b71a9d6f76f90
tsnm-x <tasneemsm1234@gmail.com>
2022-07-03 04:48:25 +0200

    added runes for each player

--- components/Ui/New-Components/universal/Btn/ShowMore/ShowMore.js
+++ components/Ui/New-Components/universal/Btn/ShowMore/ShowMore.js
@@ -3,12 +3,31 @@ import { BsChevronDown } from "react-icons/bs";
 import Classes from "./ShowMore.module.css";
 
 const ShowMore = (props) => {
-  const ClickHandler = () => {
-        props?.click();
-    };
-
+    function requestHandler(res) {
+		if (res?.status === 200) {
+			dispatch(
+				profileAction.setMoreMatches({
+					profile: res.data.matches,
+				})
+			);
+		}
+	}
+	const getMoreMatches = () => {
+		if (start === 100) {
+			return;
+		}
+		sendRequest(
+			{
+				url: "/summonerByName",
+				method: "POST",
+				body: { region, summonerName, start },
+			},
+			requestHandler
+		);
+		setStart(start + 10);
+	};
     return (
-        <button onClick={ClickHandler} className={`${Classes.BtnWrap}`}>
+        <button onClick={getMoreMatches} className={`${Classes.BtnWrap}`}>
             <h3 className={` sf-bold-40 ${Classes.BtnTxt}`}>Show more</h3>
             <BsChevronDown className=" text-[20px]  text-white mx-auto " />
         </button>
