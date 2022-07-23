000fc02d07d90a82a6a19396ab61eb14b46ec2e9
Mohd Rejoan <wearecrx@gmail.com>
2022-07-24 02:09:33 +0600

    btns section complete

--- pages/summoner/[region]/[summonerName]/[matchId]/index.js
+++ pages/summoner/[region]/[summonerName]/[matchId]/index.js
@@ -1,22 +1,20 @@
 import React from "react";
-import HeaderWithSearchbar from '../../../../../components/shared/New-Componets/HeaderWithSearchbar/HeaderWithSearchbar';
+import HeaderWithSearchbar from "../../../../../components/shared/New-Componets/HeaderWithSearchbar/HeaderWithSearchbar";
 import ProfileIntro from "../../../../../components/Ui/New-Components/Analytic_Page/ProfileIntro/ProfileIntro";
 import AnalyticsViewBtns from "../../../../../components/Ui/New-Components/Analytic_Page/AnalyticsViewBtns/AnalyticsViewBtns";
-
-
-
-
-
+import ProfileCompareBar from "../../../../../components/Ui/New-Components/Profile/OverviewElement/OverviewExpand/ProfileCompareBar/ProfileCompareBar";
 
 const MatchSimulator = () => {
     return (
         <>
             <HeaderWithSearchbar />
             <ProfileIntro />
-            <AnalyticsViewBtns />
+            <div className=" bg-[#140a22] mb-[100px] ">
+                <AnalyticsViewBtns />
+                <ProfileCompareBar />
+            </div>
         </>
     );
 };
 
 export default MatchSimulator;
-    
