9dfc1e099b6efc079eca1707211d480693dfc9a3
Mohd Rejoan <wearecrx@gmail.com>
2022-07-28 16:49:03 +0600

    overview card font, width, color updated

--- components/Ui/New-Components/Profile/OverviewElement/OverviewCards/CardControlBtns/CardControlBtns.js
+++ components/Ui/New-Components/Profile/OverviewElement/OverviewCards/CardControlBtns/CardControlBtns.js
@@ -1,20 +1,30 @@
-import React from 'react'
+import React from "react";
 
 const CardControlBtns = (props) => {
+    const filterMatches = (btn) => {
+        props.setSelectedMatchType(btn);
+    };
+    return (
+        <div className="">
+            {props?.ControlBtnLists.map((btnName, index) => {
+                return (
+                    <button
+                        key={index}
+                        onClick={() => filterMatches(btnName)}
+                        className={` sf-mid-14 text-center capitalize  bg-[#3e3847] rounded-5px mr-[10px] h-10  ${
+                            index ? "w-[140px] " : "w-[100px]"
+                        } ${
+                            btnName === props.selectedMatchType
+                                ? "text-light-text"
+                                : "text-grayed-text"
+                        }`}
+                    >
+                        {btnName}
+                    </button>
+                );
+            })}
+        </div>
+    );
+};
 
-  const filterMatches = (btn) => {
-    props.setSelectedMatchType(btn);
-  };
-  return (
-    <div className=''>
-      {props?.ControlBtnLists.map((btnName, index) => {
-        return (
-          <button key={index} onClick={() => filterMatches(btnName)} className={` sf-mid-14 text-center  capitalize py-[11px] px-[30px]
-          bg-buttons-gray rounded-5px mr-[5px] ${ (btnName === props.selectedMatchType) ? 'text-light-text': 'text-grayed-text'}`}>{btnName}</button>
-        )
-      })}
-    </div>
-  )
-}
-
-export default CardControlBtns
+export default CardControlBtns;
