7796a560b01f1089ae6bebd19de6ee29ee1d2063
Mohd Rejoan <wearecrx@gmail.com>
2022-08-02 11:17:52 +0600

    report btn done

--- components/Ui/New-Components/Profile/OverviewChampionBtns/OverviewChampionBtns.js
+++ components/Ui/New-Components/Profile/OverviewChampionBtns/OverviewChampionBtns.js
@@ -1,50 +1,72 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useState } from "react";
+import { BiMessage } from "react-icons/bi";
 
 const OverviewChampionBtns = (props) => {
     const { controller, currentView } = props;
     const router = useRouter();
 
     return (
-        <section className=" laptop:mt-[9px] ">
+        <section className=" laptop:mt-[9px] bg-[#1e1629] pt-[15px] ">
             <div className=" container flex ">
-                <div className=" mr-[21px] ">
-                    <Link href={{
-                        pathname:"/summoner/[region]/[summonerName]",
-                        query: {region: router?.query?.region, summonerName: router?.query?.summonerName}
-                    }}>
-                        <button
-                            onClick={() => controller("overview")}
-                            className={`laptop:sf-mid-21 laptop:capitalize 
+                {/* buttons  */}
+                <div className=" flex ">
+                    <div className=" mr-[21px] ">
+                        <Link
+                            href={{
+                                pathname: "/summoner/[region]/[summonerName]",
+                                query: {
+                                    region: router?.query?.region,
+                                    summonerName: router?.query?.summonerName,
+                                },
+                            }}
+                        >
+                            <button
+                                onClick={() => controller("overview")}
+                                className={`laptop:sf-mid-21 laptop:capitalize 
                         text-grayed-text ${
                             currentView === "overview"
                                 ? " text-accent-color"
                                 : "text-grayed-text"
                         } `}
-                        >
-                            overview
-                        </button>
-                    </Link>
-                    {currentView === "overview" ? (
-                        <div className=" bg-accent-color w-3/6 h-[1px] mx-auto mt-[2px] "></div>
-                    ) : null}
-                </div>
-                <div>
-                    <button
-                        onClick={() => controller("champPool")}
-                        className={`laptop:sf-mid-21 laptop:capitalize 
+                            >
+                                overview
+                            </button>
+                        </Link>
+                        {currentView === "overview" ? (
+                            <div className=" bg-accent-color w-full h-[4px] mx-auto mt-[20px] "></div>
+                        ) : null}
+                    </div>
+                    <div>
+                        <button
+                            onClick={() => controller("champPool")}
+                            className={` font-mazin laptop:capitalize 
                     text-grayed-text  ${
                         currentView === "champPool"
                             ? " text-accent-color"
                             : "text-grayed-text"
                     } `}
-                    >
-                        champion pool
+                        >
+                            champion pool
+                        </button>
+                        {currentView === "champPool" ? (
+                            <div className=" bg-accent-color w-full h-[4px] mx-auto mt-[20px] "></div>
+                        ) : null}
+                    </div>
+                </div>
+                {/* report text  */}
+                <div>
+                    {/* button  */}
+                    <button className=" bg-[#e3e3f2] w-[103px] h-[32px] flex items-center justify-center rounded-5px  ">
+                        {" "}
+                        <div className=" bg-white mr-2 px-[1px] py-[0.5px]  ">
+                            <BiMessage className=" text-accent-color text-[17px] " />{" "}
+                        </div>
+                        <h5 className=" sf-bold-14 text-accent-color capitalize ">
+                            report
+                        </h5>
                     </button>
-                    {currentView === "champPool" ? (
-                        <div className=" bg-accent-color w-3/6 h-[1px] mx-auto mt-[2px] "></div>
-                    ) : null}
                 </div>
             </div>
         </section>
