8811ba6a6990a5ace201f43f91282c978daa595a
Mohd Rejoan <wearecrx@gmail.com>
2022-09-03 11:14:26 +0600

    landing page highlight image complete

--- components/Ui/New-Components/Landing_Page/Main/Main.js
+++ components/Ui/New-Components/Landing_Page/Main/Main.js
@@ -1,30 +1,28 @@
 import React from "react";
 import Image from "next/image";
-import AnimatedImage from "../../../../../public/assets/new-images/landing-page/main-bg-sm.png";
 import ProfileSearch from "../ProfileSearch/ProfileSearch";
 import Logo from "../../universal/logo/Logo";
+import Classess from './Main.module.css'
+import HighlightImg from '../../../../../public/assets/new-images/landing-page/highlight-img.png'
 
 const Main = (props) => {
     return (
         <main className={` ${props.className}`}>
-            <div
-                className="
-                            container mobile:relative w-[730px] h-[454px] flex justify-center
-                             "
-            >
+            <div className=" w-[955px] h-[570px] mx-auto ">
                 {/* hightlight img  */}
-                <div
-                    className=" 
-                            z-10 absolute  opacity-[80%] laptop:w-[730px] laptop:h-[455px]  "
-                >
-                    <Image
-                        src={AnimatedImage}
-                        alt="highlight image"
-                        layout="fill"
-                    />
+                <div className=" w-full h-full ">
+                    <div className=" w-full h-[564px] relative ">
+                        <Image
+                            src={HighlightImg}
+                            alt="highlight image"
+                            layout="fill"
+                        />
+                        <div className={`${Classess.highImgMask}`}></div>
+                    </div>
                 </div>
+
                 {/* profile search  */}
-                <div className=" relative z-30 pt-[80px] ">
+                {/* <div className=" relative z-30 pt-[80px] ">
                     <div className=" block mb-[40px]  ">
                         <Logo className=" nedgen-regular-60 leading-[57px] font-[400] text-center mt-[45px] " />
                         <p className=" font-mazin font-[600] text-[#AAA0A8] text-[31px] leading-[38px] text-center flex flex-col items-center mt-[16px] mr-2  ">
@@ -33,7 +31,7 @@ const Main = (props) => {
                         </p>
                     </div>
                     <ProfileSearch className="" />
-                </div>
+                </div> */}
             </div>
         </main>
     );
